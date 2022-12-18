ac95200bda6f945418fc7dfd0397d2b49a57dde7
vanpe1t <makarov.maks.v@yandex.ru>
2022-12-18 11:31:48 +0300

    hometask

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,69 @@
+import java.util.Objects;
+
 public class Car {
+
+    private final String brand;
+    private final String model;
+    private final double engineVolume;
+    private final String color;
+    private final int year;
+    private final String country;
+
+    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Автомобиль " +
+                "марки: '" + brand + '\'' +
+                ", модель: '" + model + '\'' +
+                ", с объёмом двигателя: " + engineVolume +
+                ", цвет: '" + color + '\'' +
+                ", год: " + year +
+                ", страна производитель: '" + country + '\'';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Double.compare(car.engineVolume, engineVolume) == 0 && year == car.year && Objects.equals(brand, car.brand) && Objects.equals(model, car.model) && Objects.equals(color, car.color) && Objects.equals(country, car.country);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, model, engineVolume, color, year, country);
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
+        Car ladaGranta = new Car("Lada", "Granta", 1.7 , "желтый", 2015, "России");
+        Car audiA850LTDIQuattro = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия");
+        Car bmwZ8 = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия");
+        Car kiaSportage4thGen = new Car("Kia", "Sportage 4-го поколения", 2.4 , "красный", 2018, "Южная Корея");
+        Car hyundaiAvante = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");
+
+        System.out.println(ladaGranta);
+        System.out.println(audiA850LTDIQuattro);
+        System.out.println(bmwZ8);
+        System.out.println(kiaSportage4thGen);
+        System.out.println(hyundaiAvante);
+
 
     }
 }
+
+
